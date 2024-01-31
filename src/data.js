1ceb2e467987e1cc89313e3878f3427ac8d1df43
niiquash <qua16006@byui.edu>
2024-01-30 22:56:01 -0700

    start building membership page

--- src/data.js
+++ src/data.js
@@ -47,16 +47,4 @@ export const director = [
     imgUrl:
       "https://cdn.pixabay.com/photo/2023/04/02/11/20/ai-generated-7894424_960_720.jpg",
   },
-  {
-    name: "Nefertana Kush",
-    title: "Deputy Official",
-    imgUrl:
-      "https://cdn.pixabay.com/photo/2023/04/02/11/21/ai-generated-7894428_640.jpg",
-  },
-  {
-    name: "David Bayne",
-    title: "Deputy Official",
-    imgUrl:
-      "https://cdn.pixabay.com/photo/2023/02/04/17/49/ai-generated-7767762_960_720.jpg",
-  },
 ];
